a97ef3f6da3cace757e143a970b3dd5e4c51fcf2
Larissa Rodrigues <48097023+larissaralmeida@users.noreply.github.com>
2026-06-05 22:47:16 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Raphaella, eu amo você. Se você me ama, dá uma risadinha.</h1>
+    return <h1>Raphaella, eu amo você. Se você me ama, dá uma risadinha. ehe</h1>
 }
 
 export default Home;
