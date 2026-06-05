144e001a8eaf72a7ba5b36b72a235c8658c22b7e
Larissa Rodrigues <48097023+larissaralmeida@users.noreply.github.com>
2026-06-05 22:51:04 +0000

    atualiza a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Raphaella, eu amo você. Se você me ama, dá uma risadinha. ehe</h1>
+    return <h1>Raphaella, eu amo você. Se você me ama, dá uma risadinha. ehe Feliz 5 meses! <3</h1>
 }
 
 export default Home;
